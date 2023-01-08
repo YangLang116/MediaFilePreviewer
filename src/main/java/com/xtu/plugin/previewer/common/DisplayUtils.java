11e0691df529f8c0f62c5ebbf45360e3582f6f97
YangLang <yanglang116@gmail.com>
2023-01-08 16:08:20 +0800

    修复lottie预览大小异常

--- src/main/java/com/xtu/plugin/previewer/common/DisplayUtils.java
+++ src/main/java/com/xtu/plugin/previewer/common/DisplayUtils.java
@@ -9,9 +9,9 @@ public class DisplayUtils {
     public static Pair<Integer, Integer> getFitSize(int width, int height) {
         if (width < MAX_SIZE && height < MAX_SIZE) return new Pair<>(width, height);
         if (height == 0) return new Pair<>(0, 0);
-        int rate = width / height;
-        return width > height ?
-                new Pair<>(MAX_SIZE, MAX_SIZE / rate) : new Pair<>(MAX_SIZE * rate, MAX_SIZE);
+        double rate = width * 1.0f / height;
+        if (width > height) return new Pair<>(MAX_SIZE, (int) (MAX_SIZE / rate));
+        return new Pair<>((int) (MAX_SIZE * rate), MAX_SIZE);
     }
 
 }
